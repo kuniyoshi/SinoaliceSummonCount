c6792b4f07c2dfc310e2d7d312bb827061d21605
KUNIYOSHI Kouji <kuniyoshi.kouji@gmail.com>
2018-01-18 23:37:49 +0900

    u

--- SinoaliceSummonCount/src/Simulation.cs
+++ SinoaliceSummonCount/src/Simulation.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace SinoaliceSummonCount
@@ -9,65 +7,61 @@ namespace SinoaliceSummonCount
     public static class Simulation
     {
 
-        public static void Main(string[] args)
+        public static void Main()
         {
-            if (!args.Any())
-            {
-                throw new Exception(Usage());
-            }
-
-            var candidate = args[0];
-
-            if (!File.Exists(candidate))
-            {
-                throw new FileNotFoundException(Usage(), candidate);
-            }
-
-            var filename = candidate;
-
             Environment.GenRandom();
 
-            using (var reader = new StreamReader(filename, Encoding.UTF8))
-            {
-                var sinmaString = reader.ReadLine();
-                var text = reader.ReadToEnd();
-                
-                var sinma = SinmaBuilder.Build(sinmaString);
-                var guild = GuildBuilder.Build(text);
-                var totalTurn = 160;
+            var sinmaString = ReadLine();
+            var text = ReadToEnd();
 
-                var result = Environment.Simulate(
-                    guild: guild,
-                    totalTurnDuringBattring: totalTurn,
-                    firstSinma: sinma.First,
-                    secondSinma: sinma.Second
-                );
+            var sinma = SinmaBuilder.Build(sinmaString);
+            var guild = GuildBuilder.Build(text);
+            var totalTurn = 160;
+
+            var result = Environment.Simulate(
+                guild: guild,
+                totalTurnDuringBattring: totalTurn,
+                firstSinma: sinma.First,
+                secondSinma: sinma.Second
+            );
 
 //                Console.Out.WriteLine($"{firstSinma.Name} required {result.FirstRequiredTurnCount}");
 //                Console.Out.WriteLine($"{secondSinma.Name} required {result.SecondRequiredTurnCount}");
 
-                var score = Environment.CalculateScore(result, totalTurn);
+            var score = Environment.CalculateScore(result, totalTurn);
 //                Console.Out.WriteLine($"score: {score}");
 
-                var summary = Environment.Summarize(result.Records);
+            var summary = Environment.Summarize(result.Records);
 //                Console.Out.WriteLine($"summary: {string.Join("\n", summary)}");
 
-                var output = string.Join(
-                    "\t",
-                    result.FirstRequiredTurnCount.HasValue ? result.FirstRequiredTurnCount.Value.ToString() : "NULL",
-                    result.SecondRequiredTurnCount.HasValue ? result.SecondRequiredTurnCount.Value.ToString() : "NULL",
-                    score
-                );
-                Console.Out.WriteLine(output);
-            }
+            var output = string.Join(
+                "\t",
+                result.FirstRequiredTurnCount.HasValue ? result.FirstRequiredTurnCount.Value.ToString() : "NULL",
+                result.SecondRequiredTurnCount.HasValue ? result.SecondRequiredTurnCount.Value.ToString() : "NULL",
+                score
+            );
+            Console.Out.WriteLine(output);
 
             Environment.DeleteRandom();
         }
 
-
-        static string Usage()
+        static string ReadLine()
         {
-            return $"usage: script filename <filename>";
+            return Console.ReadLine();
+        }
+
+        static string ReadToEnd()
+        {
+            var b = new StringBuilder();
+            string s;
+
+            do
+            {
+                s = ReadLine();
+                b.AppendLine(s);
+            } while (s != null);
+
+            return b.ToString();
         }
 
     }
